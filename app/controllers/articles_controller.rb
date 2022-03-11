9480ea48ace8a30686f65040ec0e4f1acf5a4359
danmainah <danielmainawaweru@gmail.com>
2022-03-11 21:51:17 +0300

    Add edit view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
        @articles = Article.all
     end
     def new 
+        @article = Article.new
     end
     def create
         @article = Article.new(article_params)
